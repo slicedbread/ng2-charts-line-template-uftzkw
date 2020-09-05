e50517a1a580298e2362fb76503e274a6eb452d0
slicedbread <s1ic3dbr3ad@gmail.com>
2020-09-05 00:20:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-charts-line-template-uftzkw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-charts-line-template-uftzkw)
---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
